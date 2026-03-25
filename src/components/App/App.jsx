bb22ab45c0208f1233041e6d74b34ae0edcd72c3
PolluXx571 <alyapegasus@mail.ru>
2026-03-25 21:28:50 +0500

    add fetch and list users title,  use img component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import MyName from '../MyName/MyName';
 import { Email } from '../Email';
+import Useimg from '../Useimg';
 
 import './App.css';
 
 function App() {
       const [count, setCount] = useState(0);
+
       const condision = true;
       return (
             condision && (
@@ -13,6 +15,7 @@ function App() {
                         Hello react 1
                         <MyName />
                         <Email />
+                        <Useimg />
                   </div>
             )
       );
