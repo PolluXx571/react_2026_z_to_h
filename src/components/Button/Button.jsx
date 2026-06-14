86f44b8023ceba059efa9e68c07b5fd0dbd81254
PolluXx571 <alyapegasus@mail.ru>
2026-06-14 17:45:45 +0500

    styled components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,25 @@
-import styles from './Button.module.css';
+// bu component styled css icin kullanildi
 
-function Button({variant = "primary"}) {
+import styles from './Button.module.css';
+import { Buttonn, Buttonnn } from './Buttonn.styled';
+
+function Button({ variant = 'primary' }) {
       const buttonSecond = `${styles.secondary}`;
-    //   const buttonSecond = `${styles.button} ${styles.secondary}`;     // soyle iki tanede class ataya bilriz.
-    const primaryVariant = `${styles[variant]}`
+      //   const buttonSecond = `${styles.button} ${styles.secondary}`;     // soyle iki tanede class ataya bilriz.
+      const primaryVariant = `${styles[variant]}`;
       return (
             <div>
-                  <button className={styles.button}>Button</button>
+                  {/* <button className={styles.button}>Button</button>
                   <button className={buttonSecond}>Button second</button>
                   <button className={primaryVariant}>Button variant</button>
                   <button className={styles['bottom-block']}>Button block style</button>
+                  <Buttonn>Hello</Buttonn>
+                  <Buttonn $highlighted>Highlighted button</Buttonn> */}
+
+                  {/** $ isareti varsa DOM a gitmez uniq olur eger yoksa DOMA gider */}
+                  <Buttonnn $size='small'>Small</Buttonnn>
+                  <Buttonnn $size='medium'>Medium</Buttonnn>
+                  <Buttonnn $size='large'>Large</Buttonnn>
             </div>
       );
 }
