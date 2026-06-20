6d85e2dcb98b15b0848f63fe10c5a8e66ff50ab8
PolluXx571 <alyapegasus@mail.ru>
2026-06-21 00:50:59 +0500

    useEffect PART-2

--- src/components/Hooks/UseEffectExample.jsx
+++ src/components/Hooks/UseEffectExample.jsx
@@ -2,24 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 function UseEffectExample() {
       const [datas, setDatas] = useState([]);
-
+      const [count, setCount] = useState(0);
+      const addCount = () => {
+            setCount((prev) => prev + 1);
+      };
       useEffect(() => {
             async function getData() {
-                  const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10');
+                  const response = await fetch('https://retoolapi.dev/lf9NwJ/data?_limit=14');
                   const result = await response.json();
                   setDatas(result);
             }
             getData();
-      }, []);
+      }, [count]);
 
       return (
             <div>
                   <ul>
-                        
                         {datas?.map((data) => (
-                              <li key={data.id}>{data.title}</li>
+                              <li key={data.id}>{data.Column}</li>
                         ))}
                   </ul>
+                  <h1>{count}</h1>
+                  <button
+                        onClick={addCount}
+                        onMouseDown={(e) => (e.target.style.transform = 'scale(0.95)')}
+                        onMouseUp={(e) => (e.target.style.transform = 'scale(1)')}
+                        style={{ padding: '10px', border: '1px', background: 'red', transition: 'transform 0.1s' }}
+                  >
+                        click me
+                  </button>
             </div>
       );
 }
